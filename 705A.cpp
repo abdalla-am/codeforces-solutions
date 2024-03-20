f5d9add4fc24d78add470ea1adbc5c56d00392c6
Abdalla Ayman <112353203+abdalla-am@users.noreply.github.com>
2024-03-21 00:16:30 +0200

    Hulk

---
+++ 705A.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main() {
+//
+//	int n = 0;
+//	cin >> n;
+//	string output;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (i % 2 == 0 && i!=n) {
+//			output = output + "I love that ";
+//		}
+//		else if(i % 2 != 0 && i != n) {
+//			output = output + "I hate that ";
+//		}
+//		else if (i % 2 == 0 && i == n) {
+//			output = output + "I love it ";
+//		}
+//		else {
+//			output = output + "I hate it ";
+//		}
+//	}
+//	cout << output << endl;
+//}
