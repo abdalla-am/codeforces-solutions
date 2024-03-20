e20bcfc174d1ad55c07f879108965c277f29dfc3
Abdalla Ayman <112353203+abdalla-am@users.noreply.github.com>
2024-03-21 00:17:12 +0200

    I Wanna Be the Guy

---
+++ 469A.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include<set>
+//using namespace std;
+//int main() {
+//	int n;
+//	cin >> n;
+//	set<int> levels;
+//	int Q, Y;
+//	cin >> Q;
+//	for (int i = 0; i < Q; i++) {
+//		int x;
+//		cin >> x;
+//		levels.insert(x);
+//	}
+//	cin >> Y;
+//	for (int i = 0; i < Y; i++) {
+//		int w;
+//		cin >> w;
+//		levels.insert(w);
+//	}
+//	if (levels.size() == n)
+//		cout << "I become the guy.";
+//	else
+//		cout << "Oh, my keyboard!";
+//	return 0;
+//}
