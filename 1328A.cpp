518c71fb2ede2f5f1bb9d4f46e532829aaa15524
Abdalla Ayman <112353203+abdalla-am@users.noreply.github.com>
2024-03-21 00:18:11 +0200

    Divisibility Problem

---
+++ 1328A.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main() {
+//	int n;
+//	long x, y;
+//
+//
+//
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//
+//		cin >> x >> y;
+//		if (x % y == 0) {
+//			cout << "0" << endl;
+//
+//		}
+//		else {
+//			long w = x % y;
+//			cout << y - w << endl;
+//
+//		}
+//
+//	}
+//
+//}
