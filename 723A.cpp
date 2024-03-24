1caff35cc35be45b5aec09efdb93c137411895f2
Abdalla Ayman <112353203+abdalla-am@users.noreply.github.com>
2024-03-24 22:42:32 +0200

    The New Year: Meeting Friends

---
+++ 723A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int main() {
+	int arr[3];
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + 3);
+	cout << (arr[2] - arr[1]) + (arr[1] - arr[0]);
+	
+
+}
